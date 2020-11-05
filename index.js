3566fcb6bfbadde958c3796a3d41183678a516e8
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-11-05 09:50:06 -0300

    backend

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const routes = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(function(_req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(cors());
 app.get('/', (_req, res) => { res.status(200).send({deploy: 'deployed: heroku_d35758a6591c78f'})})
 app.use('/login', routes.loginRoute);
